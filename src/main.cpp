38bb9e70572463b39a69644a00df972430c904f6
unreformable1 <danieltroszczynski@wp.pl>
2023-11-02 19:35:13 +0100

    Add glm lib. Add camera to test rotations. Remove tests from SDL2 source code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include "glad/glad.h"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/mat4x4.hpp"
 #include "SDL2/SDL.h"
 
 #include <cassert>
@@ -9,9 +13,11 @@
 const char* vs_source =
     "#version 450 core\n"
     "layout(location = 0) in vec3 vPos;\n"
+    "uniform mat4 uView;\n"
+    "uniform mat4 uProj;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(vPos, 1.0);\n"
+    "   gl_Position = uProj * uView * vec4(vPos, 1.0);\n"
     "}\0";
 
 const char* fs_source =
@@ -88,7 +94,6 @@ int main()
     glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
     glVertexArrayAttribBinding(vao, 0, 0);
     glEnableVertexArrayAttrib(vao, 0);
-    glBindVertexArray(vao);
 
     GLint success = GL_TRUE;
     GLchar info_log[512];
@@ -122,7 +127,13 @@ int main()
     }
     glDeleteShader(vs);
     glDeleteShader(fs);
-    glUseProgram(program);
+    const glm::mat4 proj = glm::perspectiveRH(glm::radians(45.0f), static_cast<float>(window_w)/window_h, 0.1f, 100.0f);
+    glProgramUniformMatrix4fv(program, glGetUniformLocation(program, "uProj"), 1, GL_FALSE, glm::value_ptr(proj));
+
+    bool key_states[SDL_NUM_SCANCODES]{};
+    glm::vec3 camera_pos = glm::vec3(0, 0, 4);
+    float camera_pitch = 0;
+    float camera_yaw = 0;
 
     bool running = true;
     while(running == true)
@@ -134,18 +145,58 @@ int main()
             {
                 running = false;
             }
-            else if(e.type == SDL_KEYDOWN)
+            if(e.type == SDL_KEYDOWN)
             {
+                key_states[e.key.keysym.scancode] = 1;
+
                 if(e.key.keysym.sym == SDLK_ESCAPE)
                     running = false;
             }
+            if(e.type == SDL_KEYUP)
+            {
+                key_states[e.key.keysym.scancode] = 0;
+            }
+            if(e.type == SDL_MOUSEMOTION)
+            {
+                camera_pitch -= e.motion.yrel * 0.004f;
+                camera_yaw -= e.motion.xrel * 0.004f;
+
+                camera_pitch = glm::clamp(camera_pitch, glm::radians(-89.0f), glm::radians(89.0f));
+            }
         }
+
+        const glm::vec3 camera_forward = glm::vec3(
+            -glm::sin(camera_yaw),
+             glm::sin(camera_pitch) * glm::cos(camera_yaw),
+            -glm::cos(camera_pitch) * glm::cos(camera_yaw)
+        );
+        const glm::vec3 camera_right = glm::normalize(glm::cross(camera_forward, glm::vec3(0, 1, 0)));
+
+        const float speed = 0.1f;
+        glm::vec3 camera_vel{};
+        if(key_states[SDL_SCANCODE_W] == 1)         camera_vel += speed * camera_forward;
+        if(key_states[SDL_SCANCODE_S] == 1)         camera_vel -= speed * camera_forward;
+        if(key_states[SDL_SCANCODE_D] == 1)         camera_vel += speed * camera_right;
+        if(key_states[SDL_SCANCODE_A] == 1)         camera_vel -= speed * camera_right;
+        if(key_states[SDL_SCANCODE_SPACE] == 1)     camera_vel.y += speed;
+        if(key_states[SDL_SCANCODE_LSHIFT] == 1)    camera_vel.y -= speed;
+        camera_pos += camera_vel;
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        const glm::mat4 view = glm::lookAtRH(camera_pos, camera_pos + camera_forward, glm::vec3(0, 1, 0));
+        glProgramUniformMatrix4fv(program, glGetUniformLocation(program, "uView"), 1, GL_FALSE, glm::value_ptr(view));
+        glUseProgram(program);
+        glBindVertexArray(vao);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 
         SDL_GL_SwapWindow(window);
+
+        const GLenum error_code = glGetError();
+        if(error_code != GL_NONE)
+        {
+            std::cerr << "OpenGL error. Error code: " << std::hex << error_code << std::endl;
+        }
     }
 
     SDL_DestroyWindow(window);
