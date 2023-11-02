2aabfe97d839faee61dd08a67b42f5463cf4087e
unreformable1 <danieltroszczynski@wp.pl>
2023-11-02 03:41:52 +0100

    Add base OpenGL app - rect rendering

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,23 @@
 #include <iostream>
 
 
+
+const char* vs_source =
+    "#version 450 core\n"
+    "layout(location = 0) in vec3 vPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(vPos, 1.0);\n"
+    "}\0";
+
+const char* fs_source =
+    "#version 450 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(0.8);\n"
+    "}\0";
+
 int main()
 {
     const int window_w = 600;
@@ -44,6 +61,69 @@ int main()
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glViewport(0, 0, window_w, window_h);
 
+    GLfloat vertices[]{
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+    };
+
+    GLuint vbo;
+    glCreateBuffers(1, &vbo);
+    glNamedBufferStorage(vbo, sizeof(vertices), vertices, 0);
+
+    GLuint indices[]{
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    GLuint ebo;
+    glCreateBuffers(1, &ebo);
+    glNamedBufferStorage(ebo, sizeof(indices), indices, 0);
+
+    GLuint vao;
+    glCreateVertexArrays(1, &vao);
+    glVertexArrayVertexBuffer(vao, 0, vbo, 0, 3*sizeof(GLfloat));
+    glVertexArrayElementBuffer(vao, ebo);
+    glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
+    glVertexArrayAttribBinding(vao, 0, 0);
+    glEnableVertexArrayAttrib(vao, 0);
+    glBindVertexArray(vao);
+
+    GLint success = GL_TRUE;
+    GLchar info_log[512];
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vs_source, NULL);
+    glCompileShader(vs);
+    glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
+    if(success == GL_FALSE)
+    {
+        glGetShaderInfoLog(vs, 512, NULL, info_log);
+        std::cerr << "Could not compile vertex shader\n" << info_log << std::endl;
+    }
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fs_source, NULL);
+    glCompileShader(fs);
+    glGetShaderiv(fs, GL_COMPILE_STATUS, &success);
+    if(success == GL_FALSE)
+    {
+        glGetShaderInfoLog(fs, 512, NULL, info_log);
+        std::cerr << "Could not compile fragment shader\n" << info_log << std::endl;
+    }
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(success == GL_FALSE)
+    {
+        glGetProgramInfoLog(program, 512, NULL, info_log);
+        std::cerr << "Could not link program\n" << info_log << std::endl;
+    }
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    glUseProgram(program);
+
     bool running = true;
     while(running == true)
     {
@@ -62,6 +142,8 @@ int main()
         }
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 
         SDL_GL_SwapWindow(window);
     }
